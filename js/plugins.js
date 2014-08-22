6fbf6b2df0a69675a28d966ee5ca7e8181dfb8b7
Toph Tucker <tophtucker@gmail.com>
2014-08-22 16:15:04 -0400

    Random masonry-tiled placekittens with numbers and captions

--- js/plugins.js
+++ js/plugins.js
@@ -21,4 +21,70 @@
     }
 }());
 
-// Place any jQuery/helper plugins in here.
+// from http://stackoverflow.com/a/326076/120290
+function inIframe() {
+    try {
+        return window.self !== window.top;
+    } catch(err) {
+        return true;
+    }
+}
+
+// store query string in urlParams
+// from http://stackoverflow.com/a/2880929/120290
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
+
+// only return powers of 10; return blanks for anything else. (for log axis ticks.)
+function bbwNumberFormatLog(dolla) {
+    return (Math.round((Math.log(dolla) / Math.LN10) * 100) / 100) % 1 == 0 ? bbwNumberFormat(dolla) : "";
+}
+
+// adapted from d3.formatPrefix
+function bbwNumberFormat(dolla) {
+  var base = Math.max(1, Math.min(1e12, dolla));
+  var scaler = bbwFormatPrefix(base);
+  return parseFloat(scaler.scale(dolla).toPrecision(3))+scaler.symbol;
+}
+var bbw_formatPrefixes = [ "p", "n", "µ", "m", "", "k", "m", "b", "t" ].map(bbw_formatPrefix);
+function bbwFormatPrefix(value, precision) {
+  var i = 0;
+  if (value) {
+    if (value < 0) value *= -1;
+    if (precision) value = d3.round(value, d3_format_precision(value, precision));
+    i = 1 + Math.floor(1e-12 + Math.log(value) / Math.LN10);
+    i = Math.max(-24, Math.min(24, Math.floor((i <= 0 ? i + 1 : i - 1) / 3) * 3));
+  }
+  return bbw_formatPrefixes[4 + i / 3];
+};
+function bbw_formatPrefix(d, i) {
+  var k = Math.pow(10, Math.abs(4 - i) * 3);
+  return {
+    scale: i > 4 ? function(d) {
+      return d / k;
+    } : function(d) {
+      return d * k;
+    },
+    symbol: d
+  };
+}
+
+// Convert Excel dates into JS date objects
+// @author https://gist.github.com/christopherscott/2782634
+// @param excelDate {Number}
+// @return {Date}
+function getDateFromExcel(excelDate) {
+  // 1. Subtract number of days between Jan 1, 1900 and Jan 1, 1970, plus 1 (Google "excel leap year bug")
+  // 2. Convert to milliseconds.
+  return new Date((excelDate - (25567 + 1))*86400*1000);
+}
