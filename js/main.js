91eb3d4ef7da81be40688e5363e3499920ee828f
Toph Tucker <tophtucker@gmail.com>
2014-08-26 23:18:08 -0400

    Analytics/ads refactor, minus Comscore
    - Dumping a bunch of junk into plugins.js.
    - New `meta` global config object, passed to `analytics()`.
    - Separate social functions, like `shareTwitter(shareText)`.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
+var meta = {
+  site: "businessweek", // e.g. "businessweek"
+  author: "Dude", // e.g. "Claire Suddath"
+  type: "graphic", // e.g. "graphic"
+  section: "Lifestyle", // e.g. "Lifestyle"
+  title: "What to wear to the office this fall", // e.g. "Can women ever win at work?"
+  pubDate: "20140828", // e.g. "20140825"
+  shareText: "Omg I love clothes"
+};
+
 var data = [];
+
 var masonry;
 
+// random number generators for fixtures
 var width = d3.random.normal(350, 100);
 var height = d3.random.normal(350, 100);
 var characters = d3.random.normal(160, 60);
 var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed velit eget eros ullamcorper lobortis id ac dui. Vivamus commodo pulvinar leo, ac consectetur arcu mattis vel. Donec dapibus, orci eget ullamcorper egestas, felis ex elementum est, ut fermentum leo tortor nec felis. Nulla euismod commodo dui, sed pretium tortor luctus nec. Integer rhoncus metus enim, et vehicula nulla ullamcorper eu.";
 
+// random item fixture constructor
 function item(id) {
   var w = Math.round(width());
   var h = Math.round(height());
@@ -19,15 +32,19 @@ function item(id) {
   this.img = "http://placekitten.com/"+w+"/"+h;
 }
 
+// fill in fixtures
 for(var i=1; i<=50; i++) {
   data.push(new item(data.length));
 }
+
+///////////////////////////////////////////////////
 
 $( document ).ready(function() {
   if(inIframe()) $("body").addClass("iframed");
 
   var container = $("#container");
 
+  // fill in templates
   var templateItem = $("#template-item").html();
   var templateQuote = $("#template-quote").html();
   var templatePoll = $("#template-poll").html();
@@ -39,12 +56,21 @@ $( document ).ready(function() {
     }
   })
 
+  // MASONRY (once images load)
   imagesLoaded(container[0], function() {
     masonry = new Masonry( container[0], {
       columnWidth: 1,
       itemSelector: ".item"
     });
   });
+
+  // SOCIAL RULES EVERYTHING AROUND ME, SCREAM
+  $('.popup-twitter').click(function() {shareTwitter(meta.shareText) } );
+  $('.popup-linkedin').click(function() { shareLinkedIn(meta.shareText) } );
+  $('.popup-facebook').click(function() { shareFacebook(meta.shareText) } );
+
+  // the unbearable weight of ANALYTICS OR IT DIDN'T HAPPEN!!!
+  analytics(meta);
 
 });
 
--- js/plugins.js
+++ js/plugins.js
@@ -88,3 +88,231 @@ function getDateFromExcel(excelDate) {
   // 2. Convert to milliseconds.
   return new Date((excelDate - (25567 + 1))*86400*1000);
 }
+
+///////////////
+// ANALYTICS //
+///////////////
+
+var chartBeatModule;
+function analytics(meta) {
+  var comscoreConfig = {
+    site: meta.site, // e.g. "businessweek"
+    author: meta.author, // e.g. "Claire Suddath"
+    ctype: meta.type, // e.g. "graphic"
+    cg1: meta.section, // e.g. "Lifestyle"
+    name: meta.title, // e.g. "Can women ever win at work?"
+    pubDate: meta.pubDate // e.g. "20140825"
+  }
+
+  var urlHash = window.location.hash
+
+  // var comscore = Comscore(comscoreConfig)
+  // comscore.track(urlHash)
+  // comscore.trackPageview()
+
+  var _sf_async_config= {uid: 15087, domain: "www.businessweek.com"};
+  _sf_async_config.useCanonical = true;
+
+  _sf_async_config.sections = meta.section;
+  _sf_async_config.authors = meta.author;
+
+
+  (function(){
+    function loadChartbeat() {
+      window._sf_endpt=(new Date()).getTime();
+      var e = document.createElement('script');
+      e.setAttribute('language', 'javascript');
+      e.setAttribute('type', 'text/javascript');
+      e.setAttribute('src',
+        (("https:" == document.location.protocol) ? "https://s3.amazonaws.com/" : "http://") + "static.chartbeat.com/js/chartbeat_pub.js");
+      document.body.appendChild(e);
+    }
+    var oldonload = window.onload;
+    window.onload = (typeof window.onload != 'function') ? loadChartbeat : function() { oldonload(); loadChartbeat(); };
+  })();
+}
+
+/////////////
+// SHARING //
+/////////////
+
+function shareTwitter(shareText) {
+  event.preventDefault()
+  var textString = shareText
+  var text = encodeURIComponent(textString)
+  var width  = 575,
+      height = 400,
+      left   = ($(window).width()  - width)  / 2,
+      top    = ($(window).height() - height) / 2,
+      url    = $(this).attr('href') + text + "&url=" + document.URL,
+      opts   = 'status=1' +
+               ',width='  + width  +
+               ',height=' + height +
+               ',top='    + top    +
+               ',left='   + left;
+
+  window.open(url, 'twitter', opts);
+
+  return false;
+}
+
+function shareLinkedIn(shareText) {
+  event.preventDefault()
+  var textString = shareText
+  var text = encodeURIComponent(textString)
+  var width  = 550,
+      height = 420,
+      left   = ($(window).width()  - width)  / 2,
+      top    = ($(window).height() - height) / 2,
+      articleUrl = encodeURIComponent("http://businessweek.com"),
+      url    = $(this).attr('href') + text + "&url=" + articleUrl
+      opts   = 'status=1' +
+               ',width='  + width  +
+               ',height=' + height +
+               ',top='    + top    +
+               ',left='   + left;
+
+               console.log(url)
+
+  window.open(url, 'linkedin', opts);
+
+  return false;
+}
+
+function shareFacebook(shareText) {
+  event.preventDefault()
+  var textString = shareText
+  var text = encodeURIComponent(textString)
+  var width  = 550,
+      height = 420,
+      left   = ($(window).width()  - width)  / 2,
+      top    = ($(window).height() - height) / 2,
+      articleUrl = encodeURIComponent(document.URL),
+      url    = $(this).attr('href') + articleUrl
+      opts   = 'status=1' +
+               ',width='  + width  +
+               ',height=' + height +
+               ',top='    + top    +
+               ',left='   + left;
+
+      console.log(url)
+
+  window.open(url, 'facebook', opts);
+
+  return false;
+}
+
+////////////////
+// GOOGLE ADS //
+////////////////
+
+var ROOT_URL          = "http://www.businessweek.com/";
+var DOUBLECLICK_URL_PREFIX = "http://ad.doubleclick.net/N5262";
+var AD_SITE           = "mgh.bw.technology/global_tech_06052014";
+var AD_LAYOUT_DESC    = "";
+var AD_SUB_SITE       = "";
+var AD_ZONE           = "";
+var AD_URL            = (window.location.pathname == undefined) ? "/" : window.location.pathname.replace(/\/?$/, '/');
+var AD_INTERVAL_TO    = 450;
+var YIELDEX           = true;
+var NEGATIVE_AD_CATEGORIES = "";
+var URL_HASH          = "";
+var RANDOM_NUMBER     = ("1"+(""+Math.random()).substring(2,11));
+var AD_KEYWORD        = "";
+var _BUCKET           = "";
+var _BUCKET_GROUP     = "";
+
+var googletag = googletag || {};
+googletag.cmd = googletag.cmd || [], function () {
+  var a = document.createElement("script");
+  a.async = !0, a.type = "text/javascript", a.src = ("https:" == document.location.protocol ? "https://" : "http://") + "www.googletagservices.com/tag/js/gpt.js";
+  var b = document.getElementsByTagName("script")[0];
+  b.parentNode.insertBefore(a, b), googletag.bw_loaded = !0
+}();
+
+/*
+Copyright 2013 Michael Countis
+MIT License: http://opensource.org/licenses/MIT
+*/
+if(window.innerWidth > 895) {
+  !function(){"use strict";window.googletag=window.googletag||{},window.googletag.cmd=window.googletag.cmd||[],googletag.cmd.push(function(){if(!(googletag.hasOwnProperty("on")||googletag.hasOwnProperty("off")||googletag.hasOwnProperty("trigger")||googletag.hasOwnProperty("events"))){var a=googletag.debug_log.log,b=[],c=function(a,c,d){b.push({name:a,id:c,match:d})};c("gpt-google_js_loaded",8,/Google service JS loaded/gi),c("gpt-gpt_fetch",46,/Fetching GPT implementation/gi),c("gpt-gpt_fetched",48,/GPT implementation fetched\./gi),c("gpt-page_load_complete",1,/Page load complete/gi),c("gpt-queue_start",31,/^Invoked queued function/gi),c("gpt-service_add_slot",40,/Associated ([\w]*) service with slot ([\/\w]*)/gi),c("gpt-service_add_targeting",88,/Setting targeting attribute ([\w]*) with value ([\w\W]*) for service ([\w]*)/gi),c("gpt-service_collapse_containers_enable",78,/Enabling collapsing of containers when there is no ad content/gi),c("gpt-service_create",35,/Created service: ([\w]*)/gi),c("gpt-service_single_request_mode_enable",63,/Using single request mode to fetch ads/gi),c("gpt-slot_create",2,/Created slot: ([\/\w]*)/gi),c("gpt-slot_add_targeting",17,/Setting targeting attribute ([\w]*) with value ([\w\W]*) for slot ([\/\w]*)/gi),c("gpt-slot_fill",50,/Calling fillslot/gi),c("gpt-slot_fetch",3,/Fetching ad for slot ([\/\w]*)/gi),c("gpt-slot_receiving",4,/Receiving ad for slot ([\/\w]*)/gi),c("gpt-slot_render_delay",53,/Delaying rendering of ad slot ([\/\w]*) pending loading of the GPT implementation/gi),c("gpt-slot_rendering",5,/^Rendering ad for slot ([\/\w]*)/gi),c("gpt-slot_rendered",6,/Completed rendering ad for slot ([\/\w]*)/gi),googletag.events=googletag.events||{},googletag.on=function(a,b,c){if(!b)return this;a=a.split(" ");var d=c?b:void 0,e=c||b,f=0,g="";for(e.data=d,g=a[f=0];f<a.length;g=a[++f])(this.events[g]=this.events[g]||[]).push(e);return this},googletag.off=function(a,b){a=a.split(" ");var c=0,d="",e=0,f=function(){};for(d=a[c];c<a.length;d=a[++c])if(this.events.hasOwnProperty(d))if(b){for(e=this.events[d].length-1,f=this.events[d][e];e>=0;f=this.events[d][--e])f==b&&this.events[d].splice(e,1);0===this.events[d].length&&delete this.events[d]}else delete this.events[d];return this},googletag.trigger=function(a,b){if(!this.events[a]||0===this.events[a].length)return this;var b=b||[],c=0,d=this.events[a][c];for(d;c<this.events[a].length&&d.apply(this,[{data:d.data}].concat(b))!==!1;d=this.events[a][++c]);return this},googletag.debug_log.log=function(c,d){if(d&&d.getMessageId&&"number"==typeof d.getMessageId()){var h=Array.prototype.slice.call(arguments),i=0;for(i;i<b.length;i++)b[i].id===d.getMessageId()&&googletag.trigger(b[i].name,h)}return a.apply(this,arguments)}}})}();
+}
+
+if(window.innerWidth > 895) {
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/5262/mgh.bw.technology/global_tech_06052014', [[728, 90], [970, 66], [1, 1]], 'top').addService(googletag.pubads());
+    googletag.defineSlot('/5262/mgh.bw.technology/global_tech_06052014', [[300, 250], [300, 600], [1, 1]], 'right1').addService(googletag.pubads());
+    googletag.defineSlot('/5262/mgh.bw.technology/global_tech_06052014', [[300, 250], [1, 1]], 'right3').addService(googletag.pubads());
+    //googletag.pubads().collapseEmptyDivs();
+    googletag.pubads().enableAsyncRendering();
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
+  var global_ad_options = {
+    url: window.location.pathname,
+    krux_host: encodeURIComponent(location.hostname),
+    krux_uid: (Krux.user == undefined) ? "" : Krux.user,
+    krux_segments: (Krux.segments == undefined) ? "" : Krux.segments,
+    keyword: ((AD_KEYWORD == "") ? "" : AD_KEYWORD),
+    negative_ad_categories: (NEGATIVE_AD_CATEGORIES == "") ? [] : NEGATIVE_AD_CATEGORIES.split(",")
+  };
+
+  function displayAds() {
+    googletag.cmd.push(function() {
+      var ad_containers = document.getElementsByClassName("gpt_ad_container");
+      for (var i = 0; i < ad_containers.length; i++) {
+        var ac = ad_containers[i];
+        var ad_attributes = {
+          position: ac.getAttribute('data-position'),
+          size_array: ac.getAttribute('data-gpt-size'),
+          ad_size: ac.getAttribute('data-size'),
+          tile: ac.getAttribute('data-tile'),
+          ist: ac.getAttribute('data-tile') == "1" ? "dcopt=ist;" : "",
+          pt: ac.getAttribute('data-position'),
+          tz: ac.getAttribute('data-tz'),
+          yield_ex_string: (YIELDEX ? (ac.getAttribute('data-size') + "|" +
+            ac.getAttribute('data-position') + "|" + ac.getAttribute(
+              'data-tz') + "|" + global_ad_options.keyword) : "")
+        };
+        googletag.on && googletag.on('gpt-slot_rendered', function(e, level,
+          message, service, slot, reference) {
+          var $iframe;
+          var body_content;
+          var found_ad_response = false;
+
+          var slotId = slot.getSlotId();
+          var $slot = document.getElementById(slotId.getDomId());
+          var iframes = $slot.getElementsByTagName("iframe");
+          for (var i = 0; i < iframes.length; i++) {
+            if (!iframes[i].getAttribute('id').match(/hidden/)) {
+              $iframe = iframes[i];
+              break;
+            }
+          }
+          var $iframe_doc = $iframe.contentWindow.document;
+          var $iframe_content = $iframe_doc.getElementsByTagName("body");
+          if ($iframe_content.length > 0) {
+            body_content = $iframe_content[0].innerHTML || '';
+            found_ad_response = (body_content.replace(/^\s+|\s+$/g, '') !=
+              '');
+          }
+          if (found_ad_response) {
+            $slot.parentNode.setAttribute('style', 'height:' + $iframe.getAttribute(
+              'height') + 'px');
+            $slot.setAttribute('style', 'height:' + $iframe.getAttribute(
+              'height') + 'px');
+            $iframe.setAttribute('style', 'height:' + $iframe.getAttribute(
+              'height') + 'px');
+            $iframe.parentNode.setAttribute('style', 'height:' + $iframe.getAttribute(
+              'height') + 'px');
+          }
+
+        });
+        googletag.cmd.push(function() {
+          googletag.display(ad_attributes.position);
+        });
+      }
+    });
+  }
+  var d=window;d.attachEvent?d.attachEvent("onload",displayAds):d.addEventListener&&d.addEventListener("load",displayAds,!1);
+}
